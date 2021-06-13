f9b2fe0676acc9dd60a3c3f37c4f647259ef44c4
Galina Dimova <galina_marinova_dimova@abv.bg>
2021-06-13 22:19:20 +0300

    some changes

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -6,28 +6,20 @@ import ArrowDown from '../Arrow/ArrowDown'
 const Question = ({ props }) => {
     const { post, setPost } = useContext(AnswerContext)
     const { title, id, answer } = props
-    const [changePadding, setChangePaddind] = useState(false)
-    
-  
-    }
+   
     const handleClick = (event) => {
         event.preventDefault()
-        setChangePaddind(changePadding)
         setPost(pid => (pid === id ? -1 : id))
     }
-
-    
 
     return (
         <>
             <section>
-                <h2 onClick={handleClick} style={changePadding} >{title} {id}  </h2>
+                <h2 onClick={handleClick}>{title} {id}</h2>
                 {post === id ? <p>{answer}</p> : null}
             </section>
         </>
     )
 }
-
-
-
+    
 export default  Question
