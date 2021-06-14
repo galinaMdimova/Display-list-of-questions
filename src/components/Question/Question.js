ca0340ee2d2a6ea84233541804e044b95635a251
Galina Dimova <galina_marinova_dimova@abv.bg>
2021-06-14 16:08:10 +0300

    add more functionality

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,26 +1,47 @@
 import React, { useState, useContext } from 'react'
 import { AnswerContext } from '../AnswerProvider/AnswerProvider'
 import ArrowDown from '../Arrow/ArrowDown'
-import  './Question.css'
+import './Question.css'
+import styled from "styled-components"
+import pointer from '../../img/hand-pointer-solid.svg'
 
+const Heading = styled.h2`
+padding: ${props => props.alt ? "1.5em" : "1em"};
+background-color: #f4f4f4;
+text-align: start;
+cursor: url(${pointer}), auto;
+
+&:hover {
+    background-color:#ececec;     
+}
+`
 const Question = ({ props }) => {
     const { post, setPost } = useContext(AnswerContext)
     const { title, id, answer } = props
-    const [selected, setSelected] = useState(null)
+    const [isShown, setIsShown] = useState(false)
 
     const handleClick = (event) => {
         event.preventDefault()
-        setPost(pid => (pid === id ? -1 : id))   
+        setPost(pid => (pid === id ? -1 : id))
+        setIsShown(!isShown)    
     }
 
     return (
         <>
             <section className="section">
-                <h2 className="question" onClick={handleClick}>{title} {id}</h2>
+                <Heading alt={isShown}
+                    onClick={handleClick}>
+                    {title} {id}
+                    <ArrowDown
+    
+                        width={20}
+                        fill={'#eee'}
+                    />
+                </Heading>
                 {post === id ? <p>{answer}</p> : null}
             </section>
         </>
     )
 }
-    
-export default  Question
+
+export default Question
