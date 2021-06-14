cb50597a3f5acfac6e5f66ec89118167475f36c6
Galina Dimova <galina_marinova_dimova@abv.bg>
2021-06-14 11:05:19 +0300

    add more functionality

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -1,21 +1,22 @@
-import React, {useContext } from 'react'
+import React, { useState, useContext } from 'react'
 import { AnswerContext } from '../AnswerProvider/AnswerProvider'
-import './Question.css'
 import ArrowDown from '../Arrow/ArrowDown'
+import  './Question.css'
 
 const Question = ({ props }) => {
     const { post, setPost } = useContext(AnswerContext)
     const { title, id, answer } = props
-   
+    const [selected, setSelected] = useState(null)
+
     const handleClick = (event) => {
         event.preventDefault()
-        setPost(pid => (pid === id ? -1 : id))
+        setPost(pid => (pid === id ? -1 : id))   
     }
 
     return (
         <>
-            <section>
-                <h2 onClick={handleClick}>{title} {id}</h2>
+            <section className="section">
+                <h2 className="question" onClick={handleClick}>{title} {id}</h2>
                 {post === id ? <p>{answer}</p> : null}
             </section>
         </>
