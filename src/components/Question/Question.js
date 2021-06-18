3d7b736050b669a499eea47d1cee786eb3d2d6ab
Galina Dimova <galina_marinova_dimova@abv.bg>
2021-06-18 08:56:28 +0300

    add more functionality

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -6,34 +6,38 @@ import styled from "styled-components"
 import pointer from '../../img/hand-pointer-solid.svg'
 
 const Heading = styled.h2`
-padding: ${props => props.alt ? "1.5em" : "1em"};
+padding: ${props => props.change ? "1.5em 0.5em" : "1em 0.5em"};
 background-color: #f4f4f4;
 text-align: start;
 cursor: url(${pointer}), auto;
-
 &:hover {
     background-color:#ececec;     
 }
+
 `
 const Question = ({ props }) => {
     const { post, setPost } = useContext(AnswerContext)
     const { title, id, answer } = props
     const [isShown, setIsShown] = useState(false)
-
+    const [rotateArrow, setRotateArrow] = useState(false);
+   
     const handleClick = (event) => {
         event.preventDefault()
         setPost(pid => (pid === id ? -1 : id))
-        setIsShown(!isShown)    
+        setIsShown(!isShown) 
+        setRotateArrow(!rotateArrow)   
     }
 
     return (
         <>
             <section className="section">
-                <Heading alt={isShown}
+                <Heading 
+                    change={isShown}
                     onClick={handleClick}>
+
                     {title} {id}
-                    <ArrowDown
-    
+
+                    <ArrowDown className={`arrow ${rotateArrow ? "rotate" : ''}`}
                         width={20}
                         fill={'#eee'}
                     />
